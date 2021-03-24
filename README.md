bee00ebf0319b66ca8d18af73d1d42989fbec2f5
prashanthtr <prashanth.thattai@gmail.com>
2021-03-24 18:01:07 +0800

    first commit of tfrecord manager for DSSynths

---
+++ README.md
@@ -0,0 +1,1 @@
+# Write audio data to tfrecords format for training Neural networks
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='tfrecordManager',
+    version='0.1dev',
+    py_modules=['tfrecords'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read()
+)
